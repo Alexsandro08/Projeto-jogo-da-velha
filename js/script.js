86d801c905de744143fefe97b6d15b3a6d825d23
Alexsandro Eduardo <95712906+Alexsandro08@users.noreply.github.com>
2023-02-10 18:44:13 -0300

    testando em varios dispositivos

--- js/script.js
+++ js/script.js
@@ -29,6 +29,7 @@ for(let i= 0; i < boxes.length; i++){
         else{
             play02++
         }
+        checkWin()
 
 
         }
@@ -48,4 +49,130 @@ function checar(play01, play02){
     }
     
     return elemento
+}
+
+function checkWin(){
+
+    let bloc1 = document.querySelector("#bloco1")
+    let bloc2 = document.querySelector("#bloco2")
+    let bloc3 = document.querySelector("#bloco3")
+    let bloc4 = document.querySelector("#bloco4")
+    let bloc5 = document.querySelector("#bloco5")
+    let bloc6 = document.querySelector("#bloco6")
+    let bloc7 = document.querySelector("#bloco7")
+    let bloc8 = document.querySelector("#bloco8")
+    let bloc9 = document.querySelector("#bloco9")
+
+    if(bloc1.childNodes.length > 0 && bloc2.childNodes.length > 0 && bloc3.childNodes.length > 0){
+        let b1 = bloc1.childNodes[0].className
+        let b2 = bloc2.childNodes[0].className
+        let b3 = bloc3.childNodes[0].className
+
+        if(b1 == "x" && b2 == "x" && b3 == "x"){
+            console.log("X venceu")
+
+        } else if(b1 == "o" && b2 == "o" && b3 == "o"){
+            console.log("O venceu")
+
+        }
+        
+    }  if(bloc4.childNodes.length > 0 && bloc5.childNodes.length > 0 && bloc6.childNodes.length > 0){
+        let b4 = bloc4.childNodes[0].className
+        let b5 = bloc5.childNodes[0].className
+        let b6 = bloc6.childNodes[0].className
+
+        if(b4 == "x" && b5 == "x" && b6 == "x"){
+            console.log("X venceu")
+
+        } else if(b4 == "o" && b5 == "o" && b6 == "o"){
+            console.log("O venceu")
+
+        }
+        
+
+    } if(bloc7.childNodes.length > 0 && bloc8.childNodes.length > 0 && bloc9.childNodes.length > 0){
+        let b7 = bloc7.childNodes[0].className
+        let b8 = bloc8.childNodes[0].className
+        let b9 = bloc9.childNodes[0].className
+        
+        if(b7 == "x" && b8 == "x" && b9 == "x"){
+            console.log("X venceu")
+
+        } else if(b7 == "o" && b8 == "o" && b9 == "o"){
+            console.log("O venceu")
+
+        }
+    } if(bloc1.childNodes.length > 0 && bloc5.childNodes.length > 0 && bloc9.childNodes.length > 0){
+        let b1 = bloc1.childNodes[0].className
+        let b5 = bloc5.childNodes[0].className
+        let b9 = bloc9.childNodes[0].className
+        
+        if(b1 == "x" && b5 == "x" && b9 == "x"){
+            console.log("X venceu")
+
+        } else if(b1 == "o" && b5 == "o" && b9 == "o"){
+            console.log("O venceu")
+
+        }
+    } if(bloc3.childNodes.length > 0 && bloc5.childNodes.length > 0 && bloc7.childNodes.length > 0){
+        let b3 = bloc3.childNodes[0].className
+        let b5 = bloc5.childNodes[0].className
+        let b7 = bloc7.childNodes[0].className
+        
+        if(b3 == "x" && b5 == "x" && b7 == "x"){
+            console.log("X venceu")
+
+        } else if(b3 == "o" && b5 == "o" && b7 == "o"){
+            console.log("O venceu")
+
+        }
+    } if(bloc1.childNodes.length > 0 && bloc4.childNodes.length > 0 && bloc7.childNodes.length > 0){
+        let b1 = bloc1.childNodes[0].className
+        let b4 = bloc4.childNodes[0].className
+        let b7 = bloc7.childNodes[0].className
+        
+        if(b1 == "x" && b4 == "x" && b7 == "x"){
+            console.log("X venceu")
+
+        } else if(b1 == "o" && b4 == "o" && b7 == "o"){
+            console.log("O venceu")
+
+        }
+    }if(bloc2.childNodes.length > 0 && bloc5.childNodes.length > 0 && bloc8.childNodes.length > 0){
+        let b2 = bloc2.childNodes[0].className
+        let b5 = bloc5.childNodes[0].className
+        let b8 = bloc8.childNodes[0].className
+        
+        if(b2 == "x" && b5 == "x" && b8 == "x"){
+            console.log("X venceu")
+
+        } else if(b2 == "o" && b5 == "o" && b8 == "o"){
+            console.log("O venceu")
+
+        }
+    }if(bloc3.childNodes.length > 0 && bloc6.childNodes.length > 0 && bloc9.childNodes.length > 0){
+        let b3 = bloc3.childNodes[0].className
+        let b6 = bloc6.childNodes[0].className
+        let b9 = bloc9.childNodes[0].className
+        
+        if(b3 == "x" && b6 == "x" && b9 == "x"){
+            console.log("X venceu")
+
+        } else if(b3 == "o" && b6 == "o" && b9 == "o"){
+            console.log("O venceu")
+
+        }
+    }
+    let contador = 0
+    
+    for(let i =0; i < boxes.length; i++){
+        if(boxes[i].childNodes[0] != undefined){
+            contador++
+        }
+    }
+    if(contador == 9){
+        console.log("EMPATE")
+    }
+
+
 }
